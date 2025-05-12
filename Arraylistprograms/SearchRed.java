0bd8412fa200a57fe0d8e6b90e7c49638bd20313
srushtinb <srushtiicse@gmail.com>
2025-05-12 10:37:13 +0530

    add ArrayList files

---
+++ Arraylistprograms/SearchRed.java
@@ -0,0 +1,20 @@
+
+
+import java.util.ArrayList;
+
+public class SearchRed {
+    public static void main(String[] args) {
+        
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+
+        // Search for the color "Red"
+        if (colors.contains("Red")) {
+            System.out.println("Red is in the list!");
+        } else {
+            System.out.println("Red is not in the list.");
+        }
+    }
+}
