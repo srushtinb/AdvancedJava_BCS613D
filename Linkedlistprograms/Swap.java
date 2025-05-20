0e3f793b7850c827af85adca30f7a796eee3c59c
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:37:58 +0530

    Update Swap.java

--- Linkedlistprograms/Swap.java
+++ Linkedlistprograms/Swap.java
@@ -1,3 +1,7 @@
+/*Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+Collections.swap(l_list, 0, 2))*/
+
+
 import java.util.Collections;
 import java.util.LinkedList;
 
