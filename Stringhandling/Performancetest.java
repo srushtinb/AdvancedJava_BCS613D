bf3ba6476b21a5ff6e4439044644123357728d23
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:39:37 +0530

    Update Performancetest.java

--- Stringhandling/Performancetest.java
+++ Stringhandling/Performancetest.java
@@ -1,3 +1,9 @@
+
+/*2.b. Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for
+appending the string ‘AIET’ for 10000 times both in the StringBuffer and StringBuilder. Justify
+your answer which one is better.*/
+
+
 public class Performancetest {
     public static void main(String[] args) {
         // Measure performance of StringBuffer
