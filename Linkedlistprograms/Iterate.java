4aa354374862d66490a1d48c39f5defe65b992a2
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:33:12 +0530

    Update Iterate.java

--- Linkedlistprograms/Iterate.java
+++ Linkedlistprograms/Iterate.java
@@ -1,3 +1,7 @@
+
+/*Write a Java program to iterate through all elements in a linked list starting at the
+specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))*/
+
 import java.util.LinkedList;
 import java.util.ListIterator;
 
