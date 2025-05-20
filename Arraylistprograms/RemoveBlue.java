268d2730fa03dbd7d41e2fdb0597de509867ed54
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:29:36 +0530

    Update RemoveBlue.java

--- Arraylistprograms/RemoveBlue.java
+++ Arraylistprograms/RemoveBlue.java
@@ -1,3 +1,8 @@
+/*Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+
+
+
 import java.util.ArrayList;
 
 public class RemoveBlue {
