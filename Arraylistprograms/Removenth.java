368f1e353048bc47b2831b87deb8390a0ad1c378
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:31:43 +0530

    Update Removenth.java

--- Arraylistprograms/Removenth.java
+++ Arraylistprograms/Removenth.java
@@ -1,3 +1,8 @@
+/*Write a java program for getting different colors through ArrayList interface and delete nth
+element from the ArrayList object by using remove by index*/
+
+
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
