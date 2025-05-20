62b1daf2c12eb4e81024ceefa978f3de36b67b92
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:38:57 +0530

    Update Stringop.java

--- Stringhandling/Stringop.java
+++ Stringhandling/Stringop.java
@@ -1,3 +1,12 @@
+/* 2 a. Develop a java program for performing various string operations with different string
+handling functions directed as follows
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String
+Concatenation, String Splitting, StringBuilder Demo, String Formatting , Validate Email with
+contains( ) and startsWith() and endsWith()*/
+
+
+
 public class Stringop{
     public static void main(String[] args) {
         // String Creation and Basic Operations
