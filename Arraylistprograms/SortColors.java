4f9a5696fca2db4a505f100f92ca4befacb26acc
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-20 14:30:17 +0530

    Update SortColors.java

--- Arraylistprograms/SortColors.java
+++ Arraylistprograms/SortColors.java
@@ -1,3 +1,8 @@
+
+/*Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)*/
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 
