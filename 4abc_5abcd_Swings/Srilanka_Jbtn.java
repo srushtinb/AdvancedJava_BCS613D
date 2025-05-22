af55d5170516377e0ce711cb45a5732724b3dd63
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-22 20:30:19 +0530

    Update Srilanka_Jbtn.java

--- 4abc_5abcd_Swings/Srilanka_Jbtn.java
+++ 4abc_5abcd_Swings/Srilanka_Jbtn.java
@@ -1,3 +1,9 @@
+/*4b. Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is
+pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
+event handling mechanism with addActionListener( ).*/
+
+
+
 package swings;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
