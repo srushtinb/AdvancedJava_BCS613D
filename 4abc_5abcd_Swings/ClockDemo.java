4e3cb6ffc873ece30ca45ed207c1b611b8052b9d
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-22 20:27:57 +0530

    Update ClockDemo.java

--- 4abc_5abcd_Swings/ClockDemo.java
+++ 4abc_5abcd_Swings/ClockDemo.java
@@ -1,3 +1,8 @@
+/*4c. Develop a Swing program in Java to display a message “Digital Clock is pressed” or “Hour
+Glass is pressed” depending upon the Jbutton with image either Digital Clock or Hour Glass is
+pressed by implementing the event handling mechanism with addActionListener( ).*/
+
+
 package swings;
 
 import javax.swing.*;
