694c1b8137e11e4b347a1382fc1c5eac7f08c79e
SRUSHTI NB <srushtinb08@gmail.com>
2025-05-22 20:48:31 +0530

    Update SwingExample.java

--- 4abc_5abcd_Swings/SwingExample.java
+++ 4abc_5abcd_Swings/SwingExample.java
@@ -1,3 +1,7 @@
+/*
+/*4a. Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing
+Programming in Blue color plain font with font size of 32 using Jframe and Jlabel*/
+
 package swings;
 
 import java.awt.Color;
